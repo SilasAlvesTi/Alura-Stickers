da2bd711a56c9751530d7ada1559ae2552596ac9
SilasAlvesTi <silas.ifce.ti@gmail.com>
2023-03-27 16:10:44 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center"> Consumindo API de Filmes </h1>
+
+Projeto feito na aula 1 da imersão-java. 
---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.io.FileInputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        Properties props = new Properties();
+        FileInputStream input = new FileInputStream("config.properties");
+        props.load(input);
+        
+        String url = props.getProperty("FILMES_URL_API");
+        URI endereco = URI.create(url);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+        // pegar só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println("\u001b[1m Dados do Filme: ");
+            System.out.println("Título:" + "\u001b[37;1m \u001b[44;1m" + filme.get("title") + "\u001b[m");
+            System.out.println("Poster: " + filme.get("image"));
+            double ratingComPontoFlutuante = Double.parseDouble(filme.get("imDbRating"));
+            int rating = (int) ratingComPontoFlutuante;
+            System.out.print("Rating: ");
+            for (int i = 0; i < rating; i++) {
+                System.out.print("\u2B50");
+            }
+            System.out.println("\n");
+        }
+    }
+}
